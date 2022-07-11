c8f4f6d24e9a9082c80eb047e947623e93a52c19
sandwichfe <1344382941@qq.com>
2022-07-11 13:24:20 +0800

    模拟键盘输入

---
+++ src/com/company/Demo1.java
@@ -0,0 +1,75 @@
+package com.company;
+
+/**
+ * @description:
+ * @author lww
+ * @since 2022/7/11 10:21
+ */
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Demo1{
+    public static void main (String []args) {
+        new Login();
+    }
+}
+
+class Login extends JFrame implements ActionListener{
+    JPanel jp3;
+    JButton login,exit;
+    Color color = new Color(255,255,255);
+    public Login() {
+        login=new JButton("next");
+        login.addActionListener(this);
+        exit=new JButton("exit");
+        exit.addActionListener(this);
+
+        jp3=new JPanel();
+        jp3.setBackground(color);
+        //背景颜色  大小
+        login.setBackground(color);
+        login.setSize(50,60);
+        exit.setBackground(color);
+        exit.setSize(50,60);
+
+
+        // 边框
+        //login.setBorder(null);
+        //exit.setBorder(null);
+
+        jp3.add(login);
+        jp3.add(exit);
+
+        // content背景颜色
+        this.getContentPane().setBackground(Color.WHITE);
+        this.setLayout(new FlowLayout());
+        this.add(jp3);
+
+        this.toFront();
+        this.setResizable(false);
+        this.setSize(400,80);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==login) {
+            //JOptionPane.showMessageDialog(null, "ok");
+            System.out.println("ctrl 3");
+            Main.aut_mobs();
+        }
+        if(e.getSource()==exit) {
+            System.exit(0);
+        }
+    }
+
+}
+
+
+
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,11 @@ import java.awt.event.KeyEvent;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        aut_mobs();
+        //aut_mobs();
     }
 
-    private static void aut_mobs() {
+
+    public static void aut_mobs() {
         // 创建Robot对象
         Robot robot = null;
         try {
@@ -18,41 +18,26 @@ public class Main {
         } catch (AWTException e) {
             e.printStackTrace();
         }
-
         //判断是否nullaaaa
-        if(robot == null) {
+        if (robot == null) {
             return;//如果是空结束程序
-        }else {
-            //如果不是空开始跑程序
-
+        } else {
             //1、开始时为了安全等待5秒，这是为了程序运行起来之后给用户5秒时间将操作焦点准备好
-            robot.delay(5000);
+            robot.delay(2000);
 
-            //开始循环做左右移动操作
-            int i = 0;//0左A，1右D
-            int j = 0;//按键步长
-            while (true) {
-                if(i == 0) {
-                    while(j < 50) {
-                        robot.keyPress(KeyEvent.VK_D);//模拟按下D
-                        j++;
-                    }
-                    robot.keyRelease(KeyEvent.VK_D);//释放D，普通字母键可以不经历这一步，但是其他的如Ctrl等其他键，如果不释放会造成冲突的bug，影响效果
-                    i++;
-                    j=0;
-                }else {
-                    while(j < 50) {
-                        robot.keyPress(KeyEvent.VK_A);
-                        j++;
-                    }
-                    robot.keyRelease(KeyEvent.VK_A);
-                    i--;
-                    j=0;
-                }
-                //下一次操作间隔为1分钟
-                robot.delay(10000);
-            }
+            //模拟按下ctrl
+            robot.keyPress(KeyEvent.VK_CONTROL);
+            // 按下 33
+            robot.keyPress(KeyEvent.VK_3);
+
+            // 释放 3
+            robot.keyRelease(KeyEvent.VK_3);
+
+            //释放D，普通字母键可以不经历这一步，但是其他的如Ctrl等其他键，如果不释放会造成冲突的bug，影响效果
+            robot.keyRelease(KeyEvent.VK_CONTROL);
+
+            //下一次操作间隔为1分钟
+            //robot.delay(10000);
         }
     }
-
 }
